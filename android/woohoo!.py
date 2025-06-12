9923450c4d85b60dbbb9cf3a3dbebf6b6496c6b7
Nattawat Aram <nattawataramedu@gmail.com>
2025-06-13 04:10:08 +0700

    Update woohoo!.py

--- android/woohoo!.py
+++ android/woohoo!.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request # เพื่อทำ api
+from flask_cors import CORS # เพื่อทำ api
 import subprocess
 import time
 import threading
@@ -7,17 +8,18 @@ app = Flask(__name__)
 last_state = None 
 
 
-def is_charging():
+def is_charging(): # เช็ค สถานะการชาร์ตของ มือถือ โดยใช้ adb
     result = subprocess.run(["adb", "shell", "dumpsys", "battery"], capture_output=True, text=True)
     output = result.stdout
     return "AC powered: true" in output or "USB powered: true" in output
 
 
-def changestatus():
+
+def changestatus(): 
     print("พาโวเสเตตัดเชน!!!!!")
 
 
-def mong():
+def mong(): # เช็ค last_state ของ adb (สถานะการชาร์ตของ มือถือ)
     global last_state
     while True:
         charging = is_charging()
@@ -30,13 +32,27 @@ def mong():
 
 
 
-@app.route("/")
-def home():
-    status_text = "good" if last_state else "⚠️ bad"
+@app.route("/") # ระบบ website(localhost)
+def home(): 
+
+    status_text = "good" if last_state else "⚠️ เหี้ย"
     return f"<h1>Power Status: {status_text}</h1>"
 
+@app.route('/api/data', methods=['GET']) # ระบบ api
+def get_data():
+    status_text = "charging" if last_state else "disconnected"
+    data = { "status": status_text }
+    return jsonify(data)
+
+#@app.route("/มายมึงแต้")     
+#def check():
+    #status_text = "good" if last_state else "⚠️ เหี้ย"
+    #return render_template("check.html", INPUT_NAME_1 = status_text)
+
+    #  อันนี้ลองทำให้มัน edit html โดยตรงแต่ลืมว่าเรามี api อยู่แล้ว (แฮะๆ)
 
 if __name__ == "__main__":
     threading.Thread(target=mong, daemon=True).start()
+    CORS(app)
 
     app.run(host="0.0.0.0", port=5000)
